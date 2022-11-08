78bc96abc9663be6c59cb19ec21b72c45f601577
Blake C <bcortri1@kent.edu>
2022-11-07 19:27:24 -0500

    Flask functions and routes complete

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,34 @@
 # Put your app in here.
+from flask import Flask, request
+import operations as op
+app = Flask(__name__)
+
+@app.route("/math/<path>")
+def catch_all(path=None):
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    return str(eval(f"op.{path}({a},{b})"))
+
+@app.route("/add")
+def add():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    return str(op.add(a,b))
+
+@app.route("/sub")
+def sub():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    return str(op.sub(a,b))
+
+@app.route("/mult")
+def mult():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    return str(op.mult(a,b))
+
+@app.route("/div")
+def div():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    return str(op.div(a,b))
